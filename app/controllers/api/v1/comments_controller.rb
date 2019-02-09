bba487fdc38441f6de4f60c18e78ff1be559d92c
Victor Giraldes <victorpgiraldes@gmail.com>
2019-02-09 18:35:15 -0300

    Generating controllers for model comment

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,24 @@
-class Api::V1::CommentsController < ApplicationController
+class Api::V1::CommentsController < Api::V1::BaseController
+
+  respond_to :json
+
+  def index
+    comments = Comment.All
+  end
+
+  def create
+    comment = Comment.new(comments_params)
+    if comment.save
+      render json: comment, status: 201
+    else
+      render json: { errors: comment.errors }, status: 422
+    end
+  end
+
+  private
+
+  def comments_params
+      params.require(:comment).permit(:text, :user_id, :event_id)
+  end
+
 end
